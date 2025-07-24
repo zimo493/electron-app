a801a960d447403dafcee961ae9dc86fc747fa0e
zimo493 <2081182432@qq.com>
2025-07-24 14:05:34 +0800

    refactor(electron): 更新预加载脚本注释

--- electron/preload.js
+++ electron/preload.js
@@ -1,5 +1,6 @@
-// 所有Node.js API都可以在预加载过程中使用。
-// 它拥有与Chrome扩展一样的沙盒。
+/**
+ * 预加载脚本，用于在渲染进程中访问 Node.js API。
+ */
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
